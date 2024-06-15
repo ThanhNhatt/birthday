88c6b3ecd91906093de7477c8d2317cc87cf9074
your name <thanhnhathc2004@gnmail.com>
2024-06-15 22:50:11 +0700

    new commit

--- Downloads/birthday-master/config.js
+++ Downloads/birthday-master/config.js
@@ -2,6 +2,16 @@
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    imgs: {
+        "lốp du~~": "./imgs/bd.png",
+        "Chào em~~": "./imgs/bd1.png",
+        "anh đứng đây từ chiều": "./imgs/bd2.png",
+        "lo học hành đừng lấy chồng sớm": "./imgs/bd3.png",
+        "kiếm thật nhiều tiền": "./imgs/bd4.png",
+        "giựt hạc bổng nha mi": "./imgs/bd5.png",
+        
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
     texts: [
         "Chào em",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "anh đứng đây từ chiều",  // 同上...
@@ -24,10 +34,6 @@ var config = {
      * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
-    imgs: {
-        "心爱的小可爱": "./imgs/bd.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
-    },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "len  ",
