12def0cbc5c6a03ecb861ec9a8fc74c04777ecb9
your name <thanhnhathc2004@gnmail.com>
2024-06-15 17:54:18 +0700

    new commit

--- Downloads/birthday-master/config.js
+++ Downloads/birthday-master/config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "chúc bạn tủi mới",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "cầu tình đc tài",  // 同上...
-        "cầu tài đc tình",
-        "háp biu bớt đê",
-        "giàu là đc",
-        "hc giỏ",
-        "hết biết lói gì",
-        "quan trọng là bộ lòng",
-        "qqqqqqqqqqqqqqqqqqqqqqq",
-        "aaaaaaaaaaaaaaaaaaa",
-        "YAMI~~",
+        "Chào em",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "anh đứng đây từ chiều",  // 同上...
+        "chúc e tủi mới sớm trả hết nợ",
+        "lo học hành đừng lấy chồng sớm",
+        "xinh đệp",
+        "hộc giỏi",
+        "gầm cao máy thoáng",
+        "kiếm thật nhiều tiền",
+        "ko chửi mình nx hì",
+        "giựt hạc bổng nha mi",
+        "lốp du~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +25,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/thao.png",
+        "心爱的小可爱": "./imgs/bd.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "Bật đèn đee",
-        play: "nhạc mạnh á, ",
-        bannar_coming: "click típ",
-        balloons_flying: "thiếu chi rứa",
-        cake_fadein: "thiếu cc",
-        light_candle: "dô",
-        wish_message: "2 3 dô",
-        story: "nhén",
+        turn_on: "len  ",
+        play: "let go",
+        bannar_coming: "let go",
+        balloons_flying: "letgo",
+        cake_fadein: "let gô",
+        light_candle: "chiến th",
+        wish_message: "tiếp tục",
+        story: "anh mún lói là",
     }
 };
